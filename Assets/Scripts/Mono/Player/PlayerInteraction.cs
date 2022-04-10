f017afa852faf8308849714c815d84fdad7bb0b4
Diogo Figueiredo <daof2001@gmail.com>
2022-04-10 02:22:59 +0100

    pain

--- Assets/Scripts/Mono/Player/PlayerInteraction.cs
+++ Assets/Scripts/Mono/Player/PlayerInteraction.cs
@@ -41,8 +41,9 @@ namespace Stargaze.Mono.Player
         {
             RaycastHit hit;
             var camTransform = _playerCamera.transform;
+            Ray ray = new Ray(camTransform.position, camTransform.forward);
 
-            bool hitSomething = Physics.Raycast(camTransform.position, camTransform.forward, out hit, rayDistance);
+            bool hitSomething = Physics.Raycast(ray, out hit, rayDistance);
 
             if (hitSomething)
             {

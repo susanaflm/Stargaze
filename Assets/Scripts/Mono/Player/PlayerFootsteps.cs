a9c43d07e620d0c26c0a38df4d9648b5b706c415
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2022-06-01 11:18:29 +0100

    Added animation events for footstep sounds

---
+++ Assets/Scripts/Mono/Player/PlayerFootsteps.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Stargaze.Mono.Player
+{
+    public class PlayerFootsteps : MonoBehaviour
+    {
+        private void Step()
+        {
+            // TODO: Play step sound
+        }
+        
+        private void Land()
+        {
+            // TODO: Play land sound
+        }
+    }
+}
